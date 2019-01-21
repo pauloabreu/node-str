c954018fb4f9e89c471fbd43866a615485910157
Paulo Abreu <cabasafado2@hotmail.com>
2019-01-21 16:30:35 -0200

    Crud Test and body-parser test.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 const route = router.get('/', (req, res, next) => {
   res.status(200).send({
@@ -11,6 +15,25 @@ const route = router.get('/', (req, res, next) => {
   });
 });
 
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+  let id = req.params.id;
+  res.status(200).send({
+    id: id,
+    item: req.body
+  });
+});
+
+const del = router.delete('/', (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', put);
 
 module.exports = app;
