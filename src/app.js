79f5b0d6b9ee6011dd43d8c64607cdb243266f38
Paulo Abreu <cabasafado2@hotmail.com>
2019-01-22 00:33:37 -0200

    Tests and Using Controllers

--- src/app.js
+++ src/app.js
@@ -1,39 +1,19 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//Load Some Routes
+const index = require('./routes/index');
+const product = require('./routes/product');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: "Node API",
-    version: "0.0.1"
-  });
-});
 
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-  let id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body
-  });
-});
-
-const del = router.delete('/', (req, res, next) => {
-  res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', put);
+app.use('/', index);
+app.use('/products', product);
 
 module.exports = app;
